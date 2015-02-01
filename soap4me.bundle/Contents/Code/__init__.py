66cee76254eb8f2d49ed5bc98259ce3e6afa5295
Konstantin Ufimtsev <kestl1st@gmail.com>
2015-02-02 00:08:28 +0300

    Support for a new Plex API

--- soap4me.bundle/Contents/Code/__init__.py
+++ soap4me.bundle/Contents/Code/__init__.py
@@ -9,31 +9,27 @@ import calendar
 from datetime import *
 import time
 
-VERSION						= 1.3
-VIDEO_PREFIX				= "/video/soap4me"
-NAME						= 'soap4me'
-ART							= 'art.png'
-ICON						= 'icon.png'
-BASE_URL					= 'http://soap4.me/'
-API_URL						= 'http://soap4.me/api/'
-LOGIN_URL					= 'http://soap4.me/login/'
-USER_AGENT					= 'xbmc for soap'
-LOGGEDIN					= False
-TOKEN						= False
-SID							= ''
-title1						= NAME
-title2						= ''
+VERSION = 2.0
+PREFIX = "/video/soap4me"
+NAME = 'soap4.me'
+ART = 'art.png'
+ICON = 'icon.png'
+BASE_URL = 'http://soap4.me/'
+API_URL = 'http://soap4.me/api/'
+LOGIN_URL = 'http://soap4.me/login/'
+USER_AGENT = 'xbmc for soap'
+LOGGEDIN = False
+TOKEN = False
+SID = ''
+title1 = NAME
+title2 = ''
+TITLE = NAME
 
 def Start():
-	Plugin.AddPrefixHandler(VIDEO_PREFIX, MainMenu, NAME, ICON, ART)
-	Plugin.AddViewGroup("PanelStream", viewMode = "PanelStream", mediaType = "items")
-	Plugin.AddViewGroup("List", viewMode = "List", mediaType = "items")
-	MediaContainer.title1 = title1
-	MediaContainer.title2 = title2
-	MediaContainer.viewGroup = "PanelStream"
-	MediaContainer.art = R(ART)
-	DirectoryItem.thumb = R(ICON)
-	VideoItem.thumb = R(ICON)
+	ObjectContainer.art = R(ART)
+	ObjectContainer.title1 = TITLE
+	DirectoryObject.thumb = R(ICON)
+
 	HTTP.CacheTime = CACHE_1HOUR
 	HTTP.Headers['User-Agent'] = USER_AGENT
 	HTTP.Headers['Accept'] = 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
@@ -86,52 +82,20 @@ def Thumb(url):
 			return Redirect(R(ICON))
 
 
-
+@handler(PREFIX, TITLE, thumb=ICON, art=ART)
 def MainMenu():
 
-	dir = MediaContainer(viewGroup="InfoList")
-	dir.Append(Function(DirectoryItem(Soaps, title='Все сериалы')))
-	dir.Append(Function(DirectoryItem(Watching, title='Я смотрю')))
-	dir.Append(Function(DirectoryItem(Unwatched, title='Новые эпизоды')))
-	dir.Append(PrefsItem('Настройки ', thumb=R('settings.png')))
+	oc = ObjectContainer()
+	oc.add(DirectoryObject(key=Callback(Soaps, title2=u'Все сериалы', filter='all'), title=u'Все сериалы'))
+	oc.add(DirectoryObject(key=Callback(Soaps, title2=u'Я смотрю', filter='watching'), title=u'Я смотрю'))
+	oc.add(DirectoryObject(key=Callback(Soaps, title2=u'Новые эпизоды', filter='unwatched'), title=u'Новые эпизоды'))
+	oc.add(PrefsObject(title=u'Настройки', thumb=R('settings.png')))
 
-	return dir
-
-
-def Soaps(sender):
-
-	logged = Login()
-	if logged == 2:
-		return MessageContainer(
-			"Ошибка",
-			"Ведите пароль и логин"
-		)
-
-	elif logged == 3:
-		return MessageContainer(
-			"Ошибка",
-			"Отказано в доступе"
-		)
-	else:
-
-		dir = MediaContainer(viewGroup='InfoList', title2 = 'список сериалов')
-		url = API_URL + 'soap/'
-		obj = GET(url)
-
-		for items in obj:
-			title = items["title"]
-			summary = items["description"]
-			poster = 'http://covers.s4me.ru/soap/big/'+items["sid"]+'.jpg'
-			rating = items["imdb_rating"]
-			summary = summary.replace('&quot;','"')
-			fan = 'http://thetvdb.com/banners/fanart/original/'+items['tvdb_id']+'-1.jpg'
-			id = items["sid"]
-			thumb=Function(Thumb, url=poster)
-			dir.Append(Function(DirectoryItem(show_seasons, title = title, summary = summary, art = fan,rating = rating, infoLabel = '', thumb = thumb), id = id, soap_title = title))
-		return dir
+	return oc
 
 
-def Watching(sender):
+@route(PREFIX+'/{filter}')
+def Soaps(title2, filter):
 
 	logged = Login()
 	if logged == 2:
@@ -147,66 +111,43 @@ def Watching(sender):
 		)
 	else:
 
-		dir = MediaContainer(viewGroup='InfoList', title2 = 'список сериалов')
-		url = API_URL + 'soap/my/'
+		dir = ObjectContainer(title2=title2.decode())
+		if filter == 'all':
+			url = API_URL + 'soap/'
+		else:
+			url = API_URL + 'soap/my/'
 		obj = GET(url)
 
 		for items in obj:
-			#Log.Debug('#####'+str(items).encode('utf-8')+'#####')
-			title = items["title"]
-			summary = items["description"]
-			poster = 'http://covers.s4me.ru/soap/big/'+items["sid"]+'.jpg'
-			rating = items["imdb_rating"]
-			summary = summary.replace('&quot;','"')
-			fan = 'http://thetvdb.com/banners/fanart/original/'+items['tvdb_id']+'-1.jpg'
-			id = items["sid"]
-			thumb=Function(Thumb, url=poster)
-			dir.Append(Function(DirectoryItem(show_seasons, title = title, summary = summary, art = fan,rating = rating, infoLabel = '', thumb = thumb), id = id, soap_title = title))
-		return dir
-
-def Unwatched(sender):
-
-	logged = Login()
-	if logged == 2:
-		return MessageContainer(
-			"Ошибка",
-			"Ведите пароль и логин"
-		)
-
-	elif logged == 3:
-		return MessageContainer(
-			"Ошибка",
-			"Отказано в доступе"
-		)
-	else:
-
-		dir = MediaContainer(viewGroup='InfoList', title2 = 'список сериалов')
-		url = API_URL + 'soap/my/'
-		obj = GET(url)
-
-		for items in obj:
-			if items["unwatched"]!=None:
-				#Log.Debug('#####'+str(items).encode('utf-8')+'#####')
-				soap_title = items["title"]
+			if filter == 'unwatched' and items["unwatched"] == None:
+				continue
+			soap_title = items["title"]
+			if filter != 'unwatched':
+				title = soap_title
+			else:
 				title = items["title"]+ " (" +str(items["unwatched"])+ ")"
-				summary = items["description"]
-				poster = 'http://covers.s4me.ru/soap/big/'+items["sid"]+'.jpg'
-				rating = items["imdb_rating"]
-				summary = summary.replace('&quot;','"')
-				fan = 'http://thetvdb.com/banners/fanart/original/'+items['tvdb_id']+'-1.jpg'
-				id = items["sid"]
-				thumb=Function(Thumb, url=poster)
-				dir.Append(Function(DirectoryItem(show_seasons, title = title, summary = summary, art = fan,rating = rating, infoLabel = '', thumb = thumb), id = id, soap_title = soap_title, unwatched = True))
+			summary = items["description"]
+			poster = 'http://covers.s4me.ru/soap/big/'+items["sid"]+'.jpg'
+			rating = float(items["imdb_rating"])
+			summary = summary.replace('&quot;','"')
+			fan = 'http://thetvdb.com/banners/fanart/original/'+items['tvdb_id']+'-1.jpg'
+			id = items["sid"]
+			thumb=Function(Thumb, url=poster)
+			#Log.Debug('#####'+str(filter).encode('utf-8')+'#####')
+			#Log.Debug('#####'+str(filter=='unwatched').encode('utf-8')+'#####')
+			dir.add(TVShowObject(key=Callback(show_seasons, id = id, soap_title = soap_title, filter = filter, unwatched = filter=='unwatched'), rating_key = str(id), title = title, summary = summary, art = fan,rating = rating, thumb = thumb))
 		return dir
 
-def show_seasons(sender, id, soap_title, unwatched = False):
 
-	dir = MediaContainer(viewGroup='InfoList', title2 = 'список сезонов')
+@route(PREFIX+'/{filter}/{id}')
+def show_seasons(id, soap_title, filter, unwatched = False):
+
+	dir = ObjectContainer(title2 = soap_title)
 	url = API_URL + 'episodes/'+id
 	data = GET(url)
 	season = {}
 	useason = {}
-	#Log.Debug('?????'+str(data).encode('utf-8')+'?????')
+	unwatched = unwatched == 'True'
 
 	if unwatched:
 		for episode in data:
@@ -224,29 +165,33 @@ def show_seasons(sender, id, soap_title, unwatched = False):
 			if int(episode['season']) not in season:
 				season[int(episode['season'])] = episode['season_id']
 
-	#Log.Debug(str(useason))
+	#Log.Debug(str(season))
 
 	for row in season:
 		#info = {}
 		if unwatched:
-			title = "%s - Season %s (%s)" % (soap_title, str(row), str(len(useason[row])))
+			#title = "%s - Season %s (%s)" % (soap_title, str(row), str(len(useason[row])))
+			title = "Сезон %s (%s)" % (row, len(useason[row]))
 		else:
-			title = "%s - Season %s" % (soap_title, str(row))
+			#title = "%s - Season %s" % (soap_title, str(row))
+			title = "Сезон %s" % (row)
 		season_id = str(row)
-		poster = "http://covers.s4me.ru/season/big/%s.jpg"%season[row]
+		poster = "http://covers.s4me.ru/season/big/%s.jpg" % season[row]
 		thumb=Function(Thumb, url=poster)
-		dir.Append(Function(DirectoryItem(show_episodes, title = title, thumb = thumb), sid = id, season = season_id, unwatched = unwatched))
+		dir.add(SeasonObject(key=Callback(show_episodes, sid = id, season = season_id, filter=filter, unwatched = unwatched), rating_key=str(row), title = title, thumb = thumb))
 	return dir
 
-def show_episodes(sender, sid, season, unwatched = False):
+@route(PREFIX+'/{filter}/{sid}/{season}', allow_sync=True)
+def show_episodes(sid, season, filter, unwatched = False):
 
-	dir = MediaContainer(viewGroup='InfoList', title2 = 'эпизоды')
+	dir = ObjectContainer(title2 = 'Season %s' % (season))
 	url = API_URL + 'episodes/'+sid
 	data = GET(url)
 	quality = Prefs["quality"]
 	sort = Prefs["sorting"]
 	#episode_names = {}
 	show_only_hd = False
+	unwatched = unwatched == 'True'
 
 	if quality == "HD":
 		for episode in data:
@@ -283,19 +228,39 @@ def show_episodes(sender, sid, season, unwatched = False):
 					poster = "http://covers.s4me.ru/season/big/%s.jpg"%row['season_id']
 					summary = row['spoiler']
 					thumb=Function(Thumb, url=poster)
-					dir.Append(Function(VideoItem(play_episode, title = title, thumb = thumb, summary = summary), sid = sid, eid = eid, ehash = ehash))
+					dir.add(EpisodeObject(key=Callback(play_episode, sid = sid, eid = eid, ehash = ehash, row=row), rating_key=row["eid"], title = title, index=int(row['episode']), thumb = thumb, summary = summary, items=[MediaObject(parts=[PartObject(key=Callback(episode_url, sid=sid, eid=eid, ehash=ehash, part=0)), PartObject(key=Callback(episode_url, sid=sid, eid=eid, ehash=ehash, part=1))])]))
 	return dir
 
 
-def play_episode(sender, sid, eid, ehash):
-	token = Dict['token']
+def play_episode(sid, eid, ehash, row, includeExtras=0, includeRelated=0, includeRelatedCount=0):
+	oc = ObjectContainer()
+	oc.add(EpisodeObject(
+		key=Callback(play_episode, sid = sid, eid = eid, ehash = ehash, row=row),
+		rating_key=row["eid"],
+		items=[MediaObject(
+			video_codec = VideoCodec.H264,
+			audio_codec = AudioCodec.AAC,
+			container = Container.MP4,
+			optimized_for_streaming = True,
+			parts = [PartObject(key=Callback(episode_url, sid=sid, eid=eid, ehash=ehash, part=0)), PartObject(key=Callback(episode_url, sid=sid, eid=eid, ehash=ehash, part=1))]
+		)]
+	))
+	return oc
 
+
+def episode_url(sid, eid, ehash, part):
+	token = Dict['token']
+	if part==1:
+		params = {"what": "mark_watched", "eid": eid, "token": token}
+		data = JSON.ObjectFromURL("http://soap4.me/callback/", params, headers = {'x-api-token': Dict['token'], 'Cookie': 'PHPSESSID='+Dict['sid']})
+		return Redirect('https://dl.dropboxusercontent.com/u/589805/20150128_234617.mp4')
+	
 	myhash = hashlib.md5(str(token)+str(eid)+str(sid)+str(ehash)).hexdigest()
 	params = {"what": "player", "do": "load", "token":token, "eid":eid, "hash":myhash}
 
 	data = JSON.ObjectFromURL("http://soap4.me/callback/", params, headers = {'x-api-token': Dict['token'], 'Cookie': 'PHPSESSID='+Dict['sid']})
 	if data["ok"] == 1:
-		return Redirect(VideoItem("http://%s.soap4.me/%s/%s/%s/" % (data['server'], token, eid, myhash)))
+		return Redirect("http://%s.soap4.me/%s/%s/%s/" % (data['server'], token, eid, myhash))
 
 def GET(url):
 	return JSON.ObjectFromURL(url, headers = {'x-api-token': Dict['token']}, cacheTime = 0)
